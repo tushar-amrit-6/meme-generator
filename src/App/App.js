38922a320a9335c75a1134c3ed505c54093b10a2
Tushar Amrit <tusharpubg02@gmail.com>
2020-10-18 00:58:44 +0530

    final commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,22 @@
-import React, { useEffect,useState } from 'react';
-import { Meme } from '../Meme/Meme'
-import styles from './styles.module.css'
-
-export function App() {
-
-const [memes, setMemes] = useState([])
-
-  useEffect(() => {
-    fetch('https://api.imgflip.com/get_memes')
-      .then(res => res.json()
-        .then(res => {
-          const memes = res.data.memes;
-          setMemes(memes);
-        }))
-  }, []);
+  
+import React from 'react';
+import { Meme } from '../Meme/Meme';
+import { Switch, Route } from 'react-router-dom';
+import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
 
 
-
+export const App = () => {
   return (
-    memes.length ? <img src={memes[0].url}/> : <></>
+    <div>
+      <h1>Meme Creator</h1>
+      <Switch>
+        <Route exact path='/'>
+          <Meme />
+        </Route>
+        <Route path='/generated'>
+          <MemeGenerated />
+        </Route>
+      </Switch>
+    </div>
   );
 }
-
