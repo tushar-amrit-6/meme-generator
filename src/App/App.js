6e121ddc41f298eb53496fabf9d2d4ffde897778
Tushar Amrit <tusharpubg02@gmail.com>
2020-10-18 00:41:28 +0530

    css and background added

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { Meme } from '../Meme/Meme'
 import styles from './styles.module.css'
 
 export function App() {
 
-  useEffect(()=>{
+const [memes, setMemes] = useState([])
+
+  useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
-    .then(res=>res.json().then(res=>{
-      console.log(res);
-    }))
-  },[])
+      .then(res => res.json()
+        .then(res => {
+          const memes = res.data.memes;
+          setMemes(memes);
+        }))
+  }, []);
+
+
 
   return (
-    <div>
-     <Meme/> 
-    </div>
+    memes.length ? <img src={memes[0].url}/> : <></>
   );
 }
 
